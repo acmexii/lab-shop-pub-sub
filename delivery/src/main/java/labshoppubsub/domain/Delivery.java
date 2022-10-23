97ccc9e6ff5689b414dd9ff34081fdd6e6e8648a
박용주 <apexacme@gmail.com>
2022-10-23 13:28:45 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/Delivery.java
@@ -0,0 +1,75 @@
+package labshoppubsub.domain;
+
+import labshoppubsub.DeliveryApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Delivery_table")
+@Data
+
+public class Delivery  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private Long orderId;
+    
+    
+    
+    
+    
+    private Long productId;
+    
+    
+    
+    
+    
+    private Integer qty;
+
+
+    public static DeliveryRepository repository(){
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(DeliveryRepository.class);
+        return deliveryRepository;
+    }
+
+
+
+
+    public static void addToDeliverylist(OrderPlaced orderPlaced){
+
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+
+         });
+        */
+
+        
+    }
+
+
+}
